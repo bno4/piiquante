8075efa57cd6c1cd1a53d3e86363d74ac23c2184
bno4 <bnduchemin@gmail.com>
2023-04-27 16:20:37 +0200

    user controllers & models

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+const userRoutes = require('./routes/user');
+
+// connexion à la base de données mongoose atlas
+mongoose.connect('mongodb+srv://bno4:mongodbB307@cluster0.l2dcq84.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
 app.use((req, res, next) => {
-    console.log('Req reçue');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-
-app.use((req, res, next) => {
-    res.json({ message: 'votre requête a bien été reçue' });
-    next();
-});
-
-app.use((req, res) => {
-    console.log('rép envoyée avec succès');
-});
+app.use('/api/auth', userRoutes);
 module.exports = app;
